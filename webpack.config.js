5f82712607fb12c6256b9028de71147571e4b31d
Francesco Rho <eleven@rhofrances.co>
2018-07-23 18:51:02 +0200

    Update setup for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,91 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = require('./config.json');
+
+// DEVELOPMENT
 module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
             }
-        }]
-    }
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            proxy: config.proxyURL,
+            files: [
+                '**/*.php'
+            ],
+            reloadDelay: 0
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "style.css",
+            chunkFilename: "chunk.css"
+        })
+    ]
+};
+
+// PRODUCTION
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            proxy: config.proxyURL,
+            files: [
+                '**/*.php'
+            ],
+            reloadDelay: 0
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "style.css",
+            chunkFilename: "chunk.css"
+        })
+    ]
 };
